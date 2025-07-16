980ed0d0ac4aaa4cb627b41b32b0d6903e5a2eab
Jose <jacvpa33@gmail.com>
2025-07-16 00:03:21 -0600

    Modificacion del proyecto y creacion de la carperta templates

--- backend/ejemplo/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.views import APIView
-from django.http import HttpResponse
-
-# Create your views here.
-
-class Class_Ejemplo(APIView):
-
-    def get(self, request):
-        return HttpResponse("Hola ejemplo")
