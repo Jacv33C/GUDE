0a3287abf59b594e197fd26dbad58e46b35e3c97
Jose <jacvpa33@gmail.com>
2025-07-17 13:57:53 -0600

    Creacion del Archivo Home e implementacion de Jinja2

--- Universidad/Aplicaciones/General/views.py
+++ Universidad/Aplicaciones/General/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Curso
 
 # Create your views here.
+
+def home(request):
+    cursos =  Curso.objects.all()
+    return render(request, "Home.html", {"cursos": cursos}) 
