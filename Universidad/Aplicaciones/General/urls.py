665ad89b29d4ac57f12b2480246e2c9b31a273d5
Jose <jacvpa33@gmail.com>
2025-07-19 10:00:44 -0600

    Implementacion de funcionalidad para creacion de materias

--- Universidad/Aplicaciones/General/urls.py
+++ Universidad/Aplicaciones/General/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  
+    path('registrarMateria/', views.registrarMateria),
+     path('materias/', views.materias, name='materias'),
 ]
--- Universidad/Aplicaciones/General/views.py
+++ Universidad/Aplicaciones/General/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Curso
 
 # Create your views here.
 
 def home(request):
+    
+    return render(request, "Home.html" ) 
+
+def registrarMateria(request):
+    codigo = request.POST['txtCodigo']
+    nombre = request.POST['txtNombre']
+    creditos = request.POST['numCreditos']
+
+    curso =     Curso.objects.create(codigo = codigo, nombre = nombre, creditos = creditos)
+    return redirect('/materias')
+
+def materias(request):
     cursos =  Curso.objects.all()
-    return render(request, "Home.html", {"cursos": cursos}) 
+    return render(request, "Materias.html",{"cursos": cursos})
