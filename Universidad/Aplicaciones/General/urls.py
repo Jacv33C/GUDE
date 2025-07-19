fe7c4852b877817795f003131a73261a839d73dd
Jose <jacvpa33@gmail.com>
2025-07-19 13:37:50 -0600

    Implementación de la funcionalidad para eliminar materias

--- Universidad/Aplicaciones/General/urls.py
+++ Universidad/Aplicaciones/General/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  
-    path('registrarMateria/', views.registrarMateria),
-     path('materias/', views.materias, name='materias'),
+    path('', views.home, name='home'),
+
+    # Todas las rutas relacionadas con materias con prefijo 'materias/'
+    path('materias/', views.materias, name='materias'),
+    path('materias/registrarMateria/', views.registrarMateria, name='registrarMateria'),
+    path('materias/eliminacionCurso/<str:codigo>/', views.eliminacionCurso, name='eliminacionCurso'),
 ]
--- Universidad/Aplicaciones/General/views.py
+++ Universidad/Aplicaciones/General/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render, redirect
 from .models import Curso
 
-# Create your views here.
-
 def home(request):
-    
-    return render(request, "Home.html" ) 
+    return render(request, "Home.html") 
 
 def registrarMateria(request):
     codigo = request.POST['txtCodigo']
     nombre = request.POST['txtNombre']
     creditos = request.POST['numCreditos']
 
-    curso =     Curso.objects.create(codigo = codigo, nombre = nombre, creditos = creditos)
-    return redirect('/materias')
+    Curso.objects.create(codigo=codigo, nombre=nombre, creditos=creditos)
+    return redirect('materias')  # usa el nombre de la URL para redirigir
+
+def eliminacionCurso(request, codigo):
+    curso = Curso.objects.get(codigo=codigo)
+    curso.delete()
+    return redirect('materias')  # corregido aquí para redirigir correctamente
 
 def materias(request):
-    cursos =  Curso.objects.all()
-    return render(request, "Materias.html",{"cursos": cursos})
+    cursos = Curso.objects.all()
+    return render(request, "Materias.html", {"cursos": cursos})
